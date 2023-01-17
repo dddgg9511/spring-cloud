0a91d5e86ba489c165acfcc73137e976ae2eafd2
dddgg9511 <ddgg9511@naver.com>
2023-01-18 00:10:44 +0900

    gateway custom filter

--- apigateway-service/src/main/java/com/example/gateway/config/FilterConfig.java
+++ apigateway-service/src/main/java/com/example/gateway/config/FilterConfig.java
@@ -1,4 +1,4 @@
-package com.example.gateway.filter;
+package com.example.gateway.config;
 
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
