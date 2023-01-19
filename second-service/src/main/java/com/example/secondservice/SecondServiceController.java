cfcda3374b3152e9e9bac047ea831f5b89f944a6
dddgg9511 <ddgg9511@naver.com>
2023-01-19 22:29:04 +0900

    spring cloud loadbalancer  - round robin  - check called server by port number

--- second-service/src/main/java/com/example/secondservice/SecondServiceController.java
+++ second-service/src/main/java/com/example/secondservice/SecondServiceController.java
@@ -1,5 +1,6 @@
 package com.example.secondservice;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestHeader;
@@ -23,7 +24,8 @@ public class SecondServiceController {
     }
 
     @GetMapping("check")
-    public String check() {
-        return "Hi, there. This is a message from Second Service.";
+    public String check(HttpServletRequest request) {
+        return String.format("Hi, there. This is a message from second Service on PORT %s.",
+                request.getServerPort());
     }
 }
